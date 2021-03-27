29b236fcd4bbd54b2c9633cd0a4fe114199b1dbd
page <artem.kokorev2005@yandex.ru>
2021-03-27 03:56:39 +0300

    skeleton

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, request, flash
+from data import db_session
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'lolpog123'
+app.config['SECRET_KEY'] = 'crimes'
 menu = [
     {'name': 'Главная', 'url': 'main'},
     {'name': 'Карта', 'url': 'map'},
@@ -10,8 +11,8 @@ menu = [
 
 
 @app.route('/')
-def index():
-    return render_template('welcome.html', menu=menu)
+def main():
+    return render_template('main.html', menu=menu)
 
 
 @app.route('/contact')
@@ -24,5 +25,26 @@ def reg():
     return render_template('registration.html')
 
 
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
+
+@app.route('/map')
+def map():
+    return render_template('map.html')
+
+
+@app.route('/add')
+def add():
+    return render_template('add.html')
+
+
+@app.route('/<number>')
+def crime(number):
+    return render_template('crime.html')
+
+
 if __name__ == '__main__':
+    db_session.global_init("db/blogs.db")
     app.run()
