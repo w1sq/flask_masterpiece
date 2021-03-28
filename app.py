fe7c833d7b3515586db901cee333b976e0cc2089
page <artem.kokorev2005@yandex.ru>
2021-03-28 03:43:46 +0300

    trying to add profile button with picture, added navigation bar to all pages etc.

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
-from flask import Flask, render_template, request, flash
+from flask import Flask, render_template, request, flash , url_for
 from data import db_session
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'crimes'
 menu = [
-    {'name': 'Главная', 'url': 'main'},
+    {'name': 'Главная', 'url': '/'},
     {'name': 'Карта', 'url': 'map'},
-    {'name': 'Обратная связь', 'url': 'contact'}
+    {'name': 'Обратная связь', 'url': 'contact'},
+    {'name': "<img src='{url_for('static', filename='img/brawl.png')}>", 'url': 'registration'}
 ]
 
 
@@ -17,34 +18,33 @@ def main():
 
 @app.route('/contact')
 def contact():
-    return render_template('contact.html')
+    return render_template('contact.html', menu=menu)
 
 
 @app.route('/registration')
 def reg():
-    return render_template('registration.html')
+    return render_template('registration.html', menu=menu)
 
 
 @app.route('/profile')
 def profile():
-    return render_template('profile.html')
+    return render_template('profile.html', menu=menu)
 
 
 @app.route('/map')
 def map():
-    return render_template('map.html')
+    return render_template('map.html', menu=menu)
 
 
 @app.route('/add')
 def add():
-    return render_template('add.html')
+    return render_template('add.html', menu=menu)
 
 
 @app.route('/<number>')
 def crime(number):
-    return render_template('crime.html')
+    return render_template('crime.html', menu=menu)
 
 
 if __name__ == '__main__':
-    db_session.global_init("db/blogs.db")
     app.run()
