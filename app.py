6c43f7f50dbfe53c519594b37a3bebfdc4c117a7
page <artem.kokorev2005@yandex.ru>
2021-04-02 21:11:25 +0300

    idk why i do this, probably bag

--- app.py
+++ app.py
@@ -1,50 +1,46 @@
-from flask import Flask, render_template, request, flash , url_for
+from flask import Flask, render_template, request, flash, url_for
 from data import db_session
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'crimes'
-menu = [
-    {'name': 'Главная', 'url': '/'},
-    {'name': 'Карта', 'url': 'map'},
-    {'name': 'Обратная связь', 'url': 'contact'},
-    {'name': "<img src='{url_for('static', filename='img/brawl.png')}>", 'url': 'registration'}
-]
+
 
 
 @app.route('/')
 def main():
-    return render_template('main.html', menu=menu)
+    return render_template('main.html')
 
 
 @app.route('/contact')
 def contact():
-    return render_template('contact.html', menu=menu)
+    return render_template('contact.html')
 
 
 @app.route('/registration')
 def reg():
-    return render_template('registration.html', menu=menu)
+    return render_template('registration.html')
 
 
 @app.route('/profile')
 def profile():
-    return render_template('profile.html', menu=menu)
+    return render_template('profile.html')
 
 
 @app.route('/map')
 def map():
-    return render_template('map.html', menu=menu)
+    return render_template('map.html')
 
 
 @app.route('/add')
 def add():
-    return render_template('add.html', menu=menu)
+    return render_template('add.html')
 
 
 @app.route('/<number>')
 def crime(number):
-    return render_template('crime.html', menu=menu)
+    return render_template('crime.html')
 
 
 if __name__ == '__main__':
+    db_session.global_init("db/blogs.db")
     app.run()
