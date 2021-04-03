896adc875f120085e89d792cb5e7c74c584ec020
page <artem.kokorev2005@yandex.ru>
2021-04-04 02:16:17 +0300

    login and reg

--- app.py
+++ app.py
@@ -5,40 +5,54 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'crimes'
 
 
-
 @app.route('/')
 def main():
-    return render_template('main.html')
+    return render_template('main.html', login=False)
 
 
 @app.route('/contact')
 def contact():
-    return render_template('contact.html')
+    return render_template('contact.html', login=True)
 
 
 @app.route('/registration')
 def reg():
-    return render_template('registration.html')
+    return render_template('registration.html', login=True)
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html', login=True)
 
 
 @app.route('/profile')
 def profile():
-    return render_template('profile.html')
+    return render_template('profile.html', login=True)
 
 
 @app.route('/map')
-def map():
-    return render_template('map.html')
+def criminalmap():
+    return render_template('map.html', login=True)
 
 
 @app.route('/add')
 def add():
-    return render_template('add.html')
+    return render_template('add.html', login=True)
 
 
-@app.route('/<number>')
+@app.route('/crimes')
+def listofcrimes():
+    return render_template('listofcrimes.html', login=True)
+
+
+@app.route('/crimes/<number>')
 def crime(number):
-    return render_template('crime.html')
+    return render_template('crime.html', number=number, login=True)
+
+
+@app.errorhandler(404)
+def pageNotFound(error):
+    return render_template('error404.html', login=True)
 
 
 if __name__ == '__main__':
